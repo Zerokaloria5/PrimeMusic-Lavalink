36b196f560ade939d7b1d0fb7b358c63a326f806
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-06-26 01:46:17 +0200

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -41,7 +41,7 @@ module.exports = {
               url: 'https://discord.gg/s2bvv7tMaz'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setImage('https://cdn.discordapp.com/attachments/1048361437464121474/1255307766847766609/CuauFBt.png?ex=667ca7f6&is=667b5676&hm=273bc108ac9c81ad02fb9cffcb67b438990fd08b30a0359f777e69fbc98269eb&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
